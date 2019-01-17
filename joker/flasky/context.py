e6d7f0a0ea46c2d1db8b2c447efbb03254ca66a0
frozflame <frozflame@outlook.com>
2019-01-17 18:08:06 +0800

    ver 0.0.1, bug-fix: Rumor getattr no return

--- joker/flasky/context.py
+++ joker/flasky/context.py
@@ -19,7 +19,7 @@ class Rumor(object):
 
     def __getattr__(self, item):
         try:
-            self.attributes[item]
+            return self.attributes[item]
         except KeyError:
             return random.randrange(10000)
 
