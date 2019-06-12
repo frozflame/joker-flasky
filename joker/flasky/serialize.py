c4239085be6e56f5a2292ab30949c54d9388a9f2
Hailong <lendoli@163.com>
2019-06-12 15:01:20 +0800

    remove indented_json_print_legacy

--- joker/flasky/serialize.py
+++ joker/flasky/serialize.py
@@ -3,7 +3,6 @@
 
 from __future__ import unicode_literals
 
-import codecs
 import datetime
 import decimal
 import json
@@ -45,16 +44,3 @@ def indented_json_print(obj, **kwargs):
             print_kwargs[k] = kwargs.pop(k)
     s = indented_json_dumps(obj, **kwargs)
     print(s, **print_kwargs)
-
-
-# might be useless
-def indented_json_print_legacy(obj, **kwargs):
-    # https://stackoverflow.com/a/12888081/2925169
-    decoder = codecs.getdecoder('unicode_escape')
-    print_kwargs = {}
-    for k in ['sep', 'end', 'file', 'flush']:
-        if k in kwargs:
-            print_kwargs[k] = kwargs.pop(k)
-    kwargs.setdefault('indent', 4)
-    s = json.dumps(obj, **kwargs)
-    print(decoder(s)[0], **print_kwargs)
