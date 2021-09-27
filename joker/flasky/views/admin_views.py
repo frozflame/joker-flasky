5e3aca4233185ea891c25fa3ee4d97d83e6c64a9
frozflame <frozflame@outlook.com>
2021-09-27 20:41:25 +0800

    msg: current_app.error_interface is not available

--- joker/flasky/views/admin_views.py
+++ joker/flasky/views/admin_views.py
@@ -10,19 +10,31 @@ from joker.flasky.viewutils import respond, respond_plain_text
 bp = Blueprint('_admin', __name__)
 
 
-@bp.route('/r')
-def admin_raise_error():
-    raise RuntimeError('error raised intentionally')
+class _EIWrapper:
+    def __init__(self, error_interface: ErrorInterface):
+        self.ei = error_interface
+
+    def respond_error_list(self):
+        error_keys = self.ei.query_recent_error_keys()
+        tags = []
+        for ek in error_keys:
+            url = flask.url_for(flask.request.url_rule.endpoint, error_key=ek)
+            tags.append(
+                f'<pre><a href="{url}">{ek}</a></pre>'
+            )
+        return ''.join(tags)
+
+    def respond_error_info(self, error_key: str):
+        if 'i' in flask.request.args:
+            return self.ei.query(error_key, human=True)
+        url = flask.url_for(
+            flask.request.url_rule.endpoint,
+            error_key=error_key, i='',
+        )
+        return self.ei.query_html(error_key, url)
 
 
-@bp.route('/g')
-def admin_g():
-    return vars(flask.g)
-
-
-@bp.route('/echo', methods=['GET', 'POST'])
-def admin_echo():
-    flask.session['a'] = 1
+def _echo():
     return {
         # '_': vars(flask.request),
         'method': flask.request.method,
@@ -46,32 +58,15 @@ def admin_site_map():
     return respond(urls)
 
 
-def _respond_error_list(ei: ErrorInterface):
-    error_keys = ei.query_recent_error_keys()
-    tags = []
-    for ek in error_keys:
-        url = flask.url_for(flask.request.url_rule.endpoint, error_key=ek)
-        tags.append(
-            f'<pre><a href="{url}">{ek}</a></pre>'
-        )
-    return ''.join(tags)
-
-
-def _respond_error_info(ei: ErrorInterface, error_key: str):
-    if 'i' in flask.request.args:
-        return ei.query(error_key, human=True)
-    url = flask.url_for(
-        flask.request.url_rule.endpoint,
-        error_key=error_key, i='',
-    )
-    return ei.query_html(error_key, url)
-
-
-@bp.route('/err/')
 @bp.route('/e/')
 @bp.route('/e/<error_key>')
+@bp.route('/err/')
+@bp.route('/err/<error_key>')
 def admin_query_error(error_key: str = None):
     ei = getattr(current_app, 'error_interface')
+    if not ei:
+        return '<pre>current_app.error_interface is not available.</pre>'
+    eiw = _EIWrapper(ei)
     if not error_key:
-        return _respond_error_list(ei)
-    return _respond_error_info(ei, error_key)
+        return eiw.respond_error_list()
+    return eiw.respond_error_info(error_key)
